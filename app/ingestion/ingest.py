d16abd111038cbe8bc55cffeb2c047207341c59f
Aman Singh <katariaamansingh1234@gmail.com>
2026-05-17 07:32:32 +0530

    Updated

--- app/ingestion/ingest.py
+++ app/ingestion/ingest.py
@@ -10,14 +10,14 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
 
-# URLs
+# for data url
 urls = [
     "https://python.langchain.com/docs/introduction/",
     "https://docs.langchain.com/oss/python/langgraph/overview",
     "https://fastapi.tiangolo.com/tutorial/"
 ]
 
-# Load docs
+
 docs = []
 
 for url in urls:
@@ -33,7 +33,7 @@ for url in urls:
 print(f"\nLoaded {len(docs)} documents")
 
 
-# Split docs
+# here split of data into chunks
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=200
@@ -43,35 +43,25 @@ chunks = text_splitter.split_documents(docs)
 
 print(f"\nCreated {len(chunks)} chunks")
 
-
-# Embeddings
+# creating embeddings here 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
-DB_PATH = r"C:\Users\Asus\OneDrive\Pictures\Desktop\Assignment1\langgraph_db"
-
-# Delete old DB
-#if os.path.exists(DB_PATH):
- #   shutil.rmtree(DB_PATH)
+DB_PATH = "langgraph_db"
 
 
-# Create DB
 vectorstore = Chroma.from_documents(
     documents=chunks,
     embedding=embeddings,
     persist_directory=DB_PATH
 )
 
-print("\nVector DB created successfully!")
-
-
-# Test retrieval
 retriever = vectorstore.as_retriever(
     search_kwargs={"k": 3}
 )
 
-results = retriever.invoke("Who Won FIfa World Cup?")
+results = retriever.invoke("What is LangGraph?")
 
 print("\nRETRIEVED RESULTS:\n")
 
